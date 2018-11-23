d8c584aa8a458733a0ac76d96c58cf8bb75da159
dzunigan <dzuniga@uma.es>
2018-11-23 16:28:56 +0100

    git fixes

--- cmake_modules/GenerateVersionDefinitions.cmake
+++ cmake_modules/GenerateVersionDefinitions.cmake
@@ -1,13 +1,13 @@
-if(Git_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
+if(Git_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/sequence_publisher/.git")
     execute_process(COMMAND
         "${GIT_EXECUTABLE}" rev-parse --short HEAD
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/sequence_publisher/"
         OUTPUT_VARIABLE GIT_COMMIT_ID
         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     execute_process(COMMAND
         "${GIT_EXECUTABLE}" log -1 --format=%ad --date=short
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/sequence_publisher/"
         OUTPUT_VARIABLE GIT_COMMIT_DATE
         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 else()
